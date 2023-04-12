528b7a233f5880e0a78ceba29d89654f6ad3b926
Jose Laurito <jigolaurito93@yahoo.com>
2023-04-12 14:12:55 -0500

    changed font and added paper image

--- main.js
+++ main.js
@@ -6,9 +6,6 @@ addToDoButton.addEventListener('click', function(){
     var paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling');
     paragraph.innerHTML = `•  ${inputField.value}`;
-    paragraph.style.fontSize = '30px';
-    paragraph.style.paddingLeft = '100px'
-    paragraph.style.marginTop = '10px'
     toDoContainer.appendChild(paragraph);
     inputField.value = '';
 
